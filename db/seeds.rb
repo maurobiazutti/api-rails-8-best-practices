1072a47e9e6d49e9b1bb0fdd8d2f048d23513ddd
mauro <mauro_biazutti@hotmail.com>
2025-01-12 01:08:57 -0300

    `Added kind model and association to contacts, updated routes and schema, and seeded data with kinds`

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,19 @@
 # Contact.new(name: "John Doe", email: "jonh@example.com", birthdate: "1990-01-01").save
 # Contact.new(name: "Jane", email: "jabe@example.com", birthdate: "2000-02-02").save
 
+kinds = %w[Amigo Comercial Conhecido]
+
+kinds.each do|kind|
+  Kind.create!(
+    description: kind
+  )
+end
+
 10.times do
   Contact.create!(
     name: Faker::Name.name,
     email: Faker::Internet.email,
-    birthdate: Faker::Date.birthday
+    birthdate: Faker::Date.birthday,
+    kind: Kind.all.sample
   )
 end
