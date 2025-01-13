b9fd6b89753206aa2d22911486394fb504044aec
mauro <mauro_biazutti@hotmail.com>
2025-01-13 18:47:35 -0300

    `Refactor ContactsController and Contact model to include nested attributes and specific serialization options`

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
 
-  accepts_nested_attributes_for :phones
+  accepts_nested_attributes_for :phones, allow_destroy: true
 end
