e54106b1c0610f243af78f49d365f278d90a2415
mauro <mauro_biazutti@hotmail.com>
2025-02-01 22:30:12 -0300

    "Revert ensure_json_request comment and restore original implementation"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  # before_action :ensure_json_request
+  before_action :ensure_json_request
   # Ensures that only JSON requests are accepted
   # If the request's Accept header does not include application/json, then
   # return a 406 status code, indicating that the request is not acceptable.
   
-  # def ensure_json_request
-  #   return if request.headers["Accept"] =~ /json/
-  #   head :not_acceptable
-  # end
+  def ensure_json_request
+    return if request.headers["Accept"] =~ /json/
+    head :not_acceptable
+  end
   
   ############### Permitted parameters ############### 
   before_action :configure_permitted_parameters, if: :devise_controller?
