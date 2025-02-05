aad153d3afe309c96f4927f1c84323ecd0b2b5ed
mauro <mauro_biazutti@hotmail.com>
2025-02-05 18:39:50 -0300

    `Refactor routes to support API versioning with Devise token authentication`

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   root "contacts#index"
-  mount_devise_token_auth_for 'User', at: 'auth'
 
-  resources :kinds
-  resources :contacts
+  namespace :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth'
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :contacts
+      resources :kinds
+    end
+
+    scope module: :v2,
+      constraints: ApiConstraints.new(version: 2, default: false) do
+      resources :contacts
+      resources :kinds
+    end
+  end
 end
